9b2e66c8632b06ecc9e814b9121256c2e21b615e
M-Oukhadjou <oukhadjoumohamed09@gmail.com>
2026-02-20 20:27:37 +0000

    Refactor CPU instruction handling for clarity

--- cpu.py
+++ cpu.py
@@ -3,11 +3,7 @@ import sys
 ram=[0]*256
 registres={"A": 0, "B": 0, "C":0,"PC": 0,"SP":230,"FLAGS":0}
 
-# INITIALISATION
-registres["SP"] = 230
-
-
-
+registres["SP"]=230
 def charger_programme(programme):
     global ram, registres
     ram[:]=[0]*256
@@ -16,57 +12,50 @@ def charger_programme(programme):
         ram[i]=programme[i]
 
 def executer(affichage):
+    nom=["A","B","C"]
     while registres["PC"] < len(ram):
         instruction_actuelle=registres["PC"]
         IR=ram[instruction_actuelle]
         registres["PC"]+=1
         match(IR):
             case(1):#addition
-                instruction_avtuelle=registres["PC"]
-                registres["B"]=ram[instruction_avtuelle]
-                registres["PC"]+=1
-                instruction_avtuelle=registres["PC"]
-                registres["C"]=ram[instruction_avtuelle]
-                registres["PC"]+=1
-                registres["A"]= registres["B"]+registres["C"]
-                affichage.insert("end",f"Addition : {registres['B']}+{registres['C']}={registres['A']}\n")
+                d,s1,s2=ram[registres["PC"]],ram[registres["PC"]+1],ram[registres["PC"]+2]
+                registres["PC"]+=3
+                registres[nom[d]]=registres[nom[s1]]+registres[nom[s2]]
+                affichage.insert("end", f"ADD:{nom[d]}={nom[s1]}+{nom[s2]} ({registres[nom[d]]})\n")
             case(2):#soustraction
-                val1=ram[registres["PC"]]
-                registres["PC"]+=1
-                val2=ram[registres["PC"]]
-                registres["PC"]+=1
-                registres["A"]=val1-val2
-                affichage.insert("end",f"Soustraction : {val1} - {val2} = {registres['A']}\n")
+                d,s1,s2=ram[registres["PC"]],ram[registres["PC"]+1],ram[registres["PC"]+2]
+                registres["PC"]+=3
+                registres[nom[d]]=registres[nom[s1]]-registres[nom[s2]]
+                affichage.insert("end", f"SUB:{nom[d]}={nom[s1]}-{nom[s2]} ({registres[nom[d]]})\n")
             case(3):#multiplication
-                instruction_avtuelle=registres["PC"]
-                registres["B"]=ram[instruction_avtuelle]
-                registres["PC"]+=1
-                instruction_avtuelle=registres["PC"]
-                registres["C"]=ram[instruction_avtuelle]
-                registres["PC"]+=1
-                registres["A"]= registres["B"]*registres["C"]
-                affichage.insert("end", f"Multiplication : {registres['B']}*{registres['C']} = {registres['A']}\n")
+                d,s1,s2=ram[registres["PC"]],ram[registres["PC"]+1],ram[registres["PC"]+2]
+                registres["PC"]+=3
+                registres[nom[d]]=registres[nom[s1]]*registres[nom[s2]]
+                affichage.insert("end", f"MULT:{nom[d]}={nom[s1]}*{nom[s2]} ({registres[nom[d]]})\n")
             case(4):#division
-                instruction_avtuelle=registres["PC"]
-                registres["B"]=ram[instruction_avtuelle]
-                registres["PC"]+=1
-                instruction_avtuelle=registres["PC"]
-                registres["C"]=ram[instruction_avtuelle]
-                registres["PC"]+=1
-                if registres["C"] == 0:
-                    affichage.insert("end","Erreur : division par zéro")
+                d,s1,s2=ram[registres["PC"]],ram[registres["PC"]+1],ram[registres["PC"]+2]
+                registres["PC"]+=3
+                if registres[nom[s2]]==0:
+                    affichage.insert("end","ERR: Div par zero\n")
                     sys.exit()
-                else:
-                    registres["A"]= registres["B"]//registres["C"]
-                    affichage.insert("end",f"Division : {registres['B']}//{registres['C']}={registres['A']}\n")
+                registres[nom[d]]=registres[nom[s1]]//registres[nom[s2]]
+                affichage.insert("end",f"DIV: {nom[d]}={nom[s1]}/{nom[s2]}({registres[nom[d]]})\n")
             case(5):#stock
-                    if registres["SP"] >= len(ram):
-                        affichage.insert("end","Stack overflow")
-                        sys.exit()
-                    else:
-                        ram[registres["SP"]]=registres["A"]
-                        registres["SP"]+=1
-                        affichage.insert("end",f"STK : Valeur {registres['A']} enregistrée à l'adresse {registres['SP']-1}\n")
+                code_reg = ram[registres["PC"]]
+                registres["PC"]+=1
+                if code_reg not in (0,1,2):
+                    affichage.insert("end", "Registre invalide pour STK\n")
+                    break
+                if registres["SP"] <= 0:
+                    affichage.insert("end", "Stack overflow\n")
+                    sys.exit()
+                registres["SP"]+=1
+                registre_nom=nom[code_reg]
+                valeur=registres[registre_nom]
+                ram[registres["SP"]]=valeur
+                affichage.insert("end",
+                    f"STK : {registre_nom} ({valeur}) enregistré à l'adresse {registres['SP']}\n")
             case(6):#load
                 valeur=ram[registres["PC"]]
                 code_reg=ram[registres["PC"]+1]
@@ -77,26 +66,20 @@ def executer(affichage):
                     registres["B"]=valeur
                 elif code_reg==2:
                     registres["C"]=valeur
-                affichage.insert("end", f"LOAD : {valeur} chargé dans Reg {code_reg}\n")
-            case(7):#add_reg
-                registres["A"]=registres["B"]+registres["C"]
-                affichage.insert("end", f"AREG : Reg B + Reg C = {registres['A']}\n")
-            case(8):#mult_reg
-                registres["A"]=registres["B"]*registres["C"]
-                affichage.insert("end",f"MREG : {registres['B']} * {registres['C']} = {registres['A']}\n")
-            case(9):#jump
+                affichage.insert("end", f"LOAD : {valeur} chargé dans Reg {nom[code_reg]}\n")
+            case(7):#jump
                 destination=ram[registres["PC"]]
                 registres["PC"]+=1
                 registres["PC"]=destination
-            case(11):#jump_zero
+            case(8):#jump_zero
                 adresse=ram[registres["PC"]]
                 registres["PC"]+=1
-                if registres["A"]<=0:
+                if registres["A"]==0:
                     registres["PC"]=adresse
-            case(10):#exit
+            case(9):#exit
                 affichage.insert("end", ">>> Fin du programme.\n")
                 break
-            case(12):
+            case(10):
                 code_reg1=ram[registres["PC"]]
                 code_reg2=ram[registres["PC"]+1]
                 registres["PC"]+=2
@@ -114,17 +97,17 @@ def executer(affichage):
                     registres["FLAGS"]=1
                 elif val1>val2:
                     registres["FLAGS"]=2
-            case(13):
+            case(11):
                 adresse=ram[registres["PC"]]
                 registres["PC"]+=1
                 if registres["FLAGS"]==0:
                     registres["PC"]=adresse
-            case(14):
+            case(12):
                 adresse=ram[registres["PC"]]
                 registres["PC"]+=1
                 if registres["FLAGS"]==1:
                     registres["PC"]=adresse
-            case(15):
+            case(13):
                 adresse=ram[registres["PC"]]
                 registres["PC"]+=1
                 if registres["FLAGS"]==2:
